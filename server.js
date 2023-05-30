bfeda964be7359c148fabf1d9fcd7030d5f129a4
marine <marinebeatrice@hotmail.fr>
2023-05-30 12:35:50 +0200

    add user model, controller and route

---
+++ server.js
@@ -0,0 +1,10 @@
+// Creation of the server
+const http = require('http');
+const app = require('./app')
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 3000);
+
