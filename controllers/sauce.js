1c500ec61eb9987b19fcfd4bccf887919c24c8fa
marine <marinebeatrice@hotmail.fr>
2023-06-14 16:39:14 +0200

    add updatesauce router and controller

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,15 +2,15 @@ const Sauce = require('../models/sauce')
 
 exports.getSauces = (req, res, next) => {
     Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
 }
 
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-}
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+};
 
 exports.createSauce = (req, res, next) => {
     const sauceObj = JSON.parse(req.body.sauce);
@@ -23,8 +23,20 @@ exports.createSauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     });
     sauce.save()
-    .then(() => {
-        res.status(201).json({message: "Sauce créée avec succès"})
-    }) 
-    .catch(error => res.status(400).json({ error }));
-}
+        .then(() => {
+            res.status(201).json({ message: "Sauce créée avec succès" })
+        })
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObj = req.file ? {
+        //if an image already exists
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    } : { ...req.body };
+
+    Sauce.updateOne({ _id: req.params.id}, { ...sauceObj, _id: req.params.id})
+    .then(() => res.status(200).json({message : 'Objet modifié!'}))
+    .catch(error => res.status(401).json({ error }));
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -6,6 +6,7 @@ const multer = require('../middleware/multer-config')
 sauceCtrl = require('../controllers/sauce')
 
 //routes 
+router.put('/:id', auth, multer, sauceCtrl.modifySauce)
 router.post('/', auth, multer, sauceCtrl.createSauce)
 router.get('/:id', auth, sauceCtrl.getOneSauce)
 router.get('/', auth, sauceCtrl.getSauces)
