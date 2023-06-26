4cf5a21ae43a1538e724106844c163536bc5e462
marine <marinebeatrice@hotmail.fr>
2023-06-26 17:15:11 +0200

    add rate limit

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -35,7 +35,6 @@ exports.modifySauce = (req, res, next) => {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     } : { ...req.body };
-
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => {
             Sauce.updateOne({ _id: req.params.id }, { ...sauceObj, _id: req.params.id })
@@ -71,19 +70,19 @@ exports.likeOrDislike = (req, res, next) => {
                 Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: +1 }, $push: { usersLiked: req.body.userId } },)
                     .then(() => res.status(200).json({ message: 'Like ajouté !' }))
                     .catch(error => res.status(400).json({ error }))
-            // if user dislikes the sauce
+                // if user dislikes the sauce
             } else if (req.body.like === -1) {
                 Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: +1 }, $push: { usersDisliked: req.body.userId } })
                     .then(() => res.status(200).json({ message: 'Dislike ajouté !' }))
                     .catch(error => res.status(400).json({ error }))
-            // if user wants to cancel like or dislike
+                // if user wants to cancel like or dislike
             } else if (req.body.like === 0) {
                 // Cancel  like
                 if (sauce.usersLiked.includes(req.body.userId)) {
                     Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId } })
                         .then(() => res.status(200).json({ message: 'Like supprimé' }))
                         .catch(error => res.status(400).json({ error }))
-                // Cancel dislike
+                    // Cancel dislike
                 } else if (sauce.usersDisliked.includes(req.body.userId)) {
                     Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId } })
                         .then((sauce) => res.status(200).json({ message: 'Dislike supprimé' }))
